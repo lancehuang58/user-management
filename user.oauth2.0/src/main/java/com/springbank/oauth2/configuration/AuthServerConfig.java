0c694dc209fc596c1ea4f1d6918db9fd686b9e2d
Lance Huang <lance.huang@deltaww.com>
2023-03-30 16:51:24 +0800

    auth server config update.

--- user.oauth2.0/src/main/java/com/springbank/oauth2/configuration/AuthServerConfig.java
+++ user.oauth2.0/src/main/java/com/springbank/oauth2/configuration/AuthServerConfig.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -42,5 +46,29 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Bean
     public JwtTokenStore jwtTokenStore() {
         return new JwtTokenStore(tokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(jwtTokenStore())
+                .accessTokenConverter(tokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll").checkTokenAccess("isAuthenticated");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient(clientId)
+                .secret(new BCryptPasswordEncoder(12).encode(clientSecret))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(tokenValidity)
+                .refreshTokenValiditySeconds(refreshTokenValidity);
+
+
     }
 }
